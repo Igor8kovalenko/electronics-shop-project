6b383366ad824d6c120a5b684c005a829aaab9a9
Dell-PC <Igorek.kovalenko.2015@mail.ru>
2023-07-04 22:18:20 +0300

    Шев все пропало!!!!!

--- src/item.py
+++ src/item.py
@@ -1,4 +1,11 @@
+import os
+
+
+import csv
+
+path = os.path.join('..', 'src', 'items.csv')
 class Item:
+
     """
     Класс для представления товара в магазине.
     """
@@ -14,10 +21,40 @@ class Item:
         :param quantity: Количество товара в магазине.
         """
 
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all = []
+        try:
+            with open(path, encoding='windows-1251', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                cls.all = [cls((row['name']), float(row['price']), int(row['quantity'])) for row in reader]
+        # return cls.all
+        except FileNotFoundError:
+            print('_Отсутствует файл item.csv_')
+
+    @classmethod
+    def item_name(cls, name):
+        if len(name) <=10:
+            name = [x for x in name.split() if x]
+            return name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.item_name(name)
+        self.__name = name
+
+    @staticmethod
+    def string_to_number(str_number):
+        return int(float(str_number))
 
 
     def calculate_total_price(self) -> float:
--- tests/1test_item.py
+++ tests/1test_item.py
@@ -1,9 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
-
 from src.item import Item
 import pytest
 import os
+
 path = os.path.join('..', 'tests', 'test_items.csv')
 
 item1 = Item("Смартфон", 10000, 20)
@@ -13,20 +13,21 @@ def test_calculate_total_price():
     item = Item("Product", 10000, 20)
     assert item.calculate_total_price() == 200000
 
+
 def test_apply_discount():
     item = Item("Product", 10000, 20)
     Item.pay_rate = 0.8  # set the discount to 20%
     item.apply_discount()
     assert item.price == 8000
 
+
 def test_name():
     item1.name = 'Смартфон'
     assert item1.name == 'Смартфон'
 
-# def test_string_to_number('a, b',[('5', 5),('5.0', 5),('5.5', 5)]):
+    # def test_string_to_number('a, b',[('5', 5),('5.0', 5),('5.5', 5)]):
 
-    # def test_string_to_number(a, result):
-    #     assert Item.string_to_number('5') == 5
-    #     assert Item.string_to_number('5.0') == 5
-    #     assert Item.string_to_number('5.5') == 5
-
+    def test_string_to_number(a, result):
+        assert Item.string_to_number('5') == 5
+        assert Item.string_to_number('5.0') == 5
+        assert Item.string_to_number('5.5') == 5
